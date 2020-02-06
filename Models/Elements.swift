baa3262d4612f949b69ae556ecd286b706af3fe8
Guy Ardito <guyardito@gmail.com>
2020-02-06 16:31:14 -0500

    add new file

---
+++ Models/Elements.swift
@@ -0,0 +1,18 @@
+//
+//  Elements.swift
+//  Digital Empire
+//
+//  Created by Guy (main) on 2/6/20.
+//  Copyright © 2020 Rich Life LLC. All rights reserved.
+//
+
+import Foundation
+
+// user can have any number of any of these
+
+enum Elements {
+	case Ad
+	case BoostedPost
+	
+	
+}
