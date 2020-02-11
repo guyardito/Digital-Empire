e1d27609136de5d871b9fbc43c4f2fa22e151d1c
guyardito <guyardito@gmail.com>
2020-02-11 17:12:49 -0500

    add new files

---
+++ Models/Outsource.swift
@@ -0,0 +1,9 @@
+//
+//  Outsource.swift
+//  Digital Empire
+//
+//  Created by Guy Ardito on 2/11/20.
+//  Copyright © 2020 Rich Life LLC. All rights reserved.
+//
+
+import Foundation
---
+++ TestSwiftUI/OutsourceView.swift
@@ -0,0 +1,21 @@
+//
+//  OutsourceView.swift
+//  Digital Empire
+//
+//  Created by Guy Ardito on 2/11/20.
+//  Copyright © 2020 Rich Life LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct OutsourceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OutsourceView_Previews: PreviewProvider {
+    static var previews: some View {
+        OutsourceView()
+    }
+}
