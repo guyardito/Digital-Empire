1358366841d4e08788f221da05e4c28eea7b9ede
guyardito <guyardito@gmail.com>
2020-02-13 10:43:52 -0500

    move contents relevant to 'CreatableItem' into their own file

--- Models/Elements.swift
+++ Models/Elements.swift
@@ -10,49 +10,6 @@ import Foundation
 
 
 
-
-
-enum CreatableItemType {
-	case Ad
-	case BlogPost
-	case Freebie
-	case Course
-	case SalesPage
-	case Website
-	case EmailCampaign
-}
-
-
-struct CreatableItem {
-	let type : CreatableItemType   // ad, blogPost, freebie, salespage, emailCampaign, course
-	let name : String
-	
-	// initialize this based skills (like copywriting)
-	var timeLeftTillCreated = 0 	// in game ticks
-}
-
-
-func timeToCreate(item:CreatableItem) -> Int {
-	
-	return 0
-}
-
-
-
-
-
-func costToImplement(moneyMaker:MoneyMakerType) -> Int {
-	
-	switch moneyMaker {
-		case .Webinar: return 100
-		
-		case .Campaign: return 30
-		
-		case .LiveEvent: return 5000
-	}
-}
-
-
 enum MoneyMakerType {
 	case Webinar
 	case Campaign
