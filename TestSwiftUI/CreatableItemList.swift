c3e0a689147eac424ea9231f01391e2fcd32e7a2
guyardito <guyardito@gmail.com>
2020-02-14 12:01:23 -0500

    just a little cleanup

--- TestSwiftUI/CreatableItemList.swift
+++ TestSwiftUI/CreatableItemList.swift
@@ -8,11 +8,13 @@
 
 import SwiftUI
 
+
 struct CreatableItemList: View {
 	
 	//@EnvironmentObject var game:GameCoordinator
-	@ObservedObject var stats:Stats  // NB need this declaration to recognize changes to the array
 	
+	@ObservedObject var stats:Stats  // NB need this declaration to recognize changes to the array
+
 	var body: some View {
 		ScrollView(.horizontal)  {
 			ZStack {
@@ -36,17 +38,9 @@ struct CreatableItemList_Previews: PreviewProvider {
 	
 	static let stats = getTestUserData()
 	
-	static let ads = [
-		CreatableItemProxy( item: Ad(name: "ad 1", dailySpend: 5, clickThru: 4, totalClicks: 2391, costPerClick: 3.25, dayStartRunning: 5, dayStartCreating:2, daysToCreate: 3) ),
-		CreatableItemProxy( item: Ad(name: "ad 2", dailySpend: 3, clickThru: 8, totalClicks: 430, costPerClick: 4.20, dayStartRunning: 20, dayStartCreating: 13, daysToCreate: 2) ),
-		CreatableItemProxy( item: Ad(name: "ad 2", dailySpend: 3, clickThru: 8, totalClicks: 430, costPerClick: 4.20, dayStartRunning: 20, dayStartCreating: 13, daysToCreate: 2) ),
-		CreatableItemProxy( item: Ad(name: "ad 2", dailySpend: 3, clickThru: 8, totalClicks: 430, costPerClick: 4.20, dayStartRunning: 20, dayStartCreating: 13, daysToCreate: 2) ),
-		CreatableItemProxy( item: Ad(name: "ad 3", dailySpend: 7, clickThru: 2, totalClicks: 3096, costPerClick: 1.75, dayStartRunning: 30, dayStartCreating: 22, daysToCreate: 1) )
-		
-	]
 
     static var previews: some View {
 		//CreatableItemList(items:ads)
-		CreatableItemList(stats:stats)
+		CreatableItemList(stats: stats)
     }
 }
