d8176f80a7c917a1ef37e6992277c42e0bd55691
Guy Ardito <guyardito@gmail.com>
2020-02-15 22:04:56 -0500

    add new files

---
+++ Models/LeadCampaign.swift
@@ -0,0 +1,9 @@
+//
+//  LeadCampaign.swift
+//  Digital Empire
+//
+//  Created by Guy (main) on 2/15/20.
+//  Copyright © 2020 Rich Life LLC. All rights reserved.
+//
+
+import Foundation
---
+++ TestSwiftUI/LeadCampaignView.swift
@@ -0,0 +1,21 @@
+//
+//  LeadCampaignView.swift
+//  Digital Empire
+//
+//  Created by Guy (main) on 2/15/20.
+//  Copyright © 2020 Rich Life LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct LeadCampaignView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LeadCampaignView_Previews: PreviewProvider {
+    static var previews: some View {
+        LeadCampaignView()
+    }
+}
